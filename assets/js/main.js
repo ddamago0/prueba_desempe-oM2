d95efae0df0a2017970dbe2e2711da029bd3bc4e
Daniel Martinez <ddamago0@gmai.com>
2026-04-27 15:59:49 -0500

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
-const date = new Date()
-document.getElementById("year").innerText = date.getFullYear()
+document.addEventListener("DOMContentLoaded", () => {
 
-// Write your Js code here 
+  // BOTÓN HAMBURGUESA
+  const menuBtn = document.getElementById("menuToggle");
+  const nav = document.getElementById("navMenu");
+
+  menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+
+  // SE CIERRE EL MENU
+const links = document.querySelectorAll("#navMenu a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+  // CAMBIO DE TEXTO (About)
+  const btn = document.getElementById("changeTextBtn");
+  const text = document.getElementById("aboutText");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      text.textContent = "Now you are interacting with my portfolio 🚀";
+    });
+  }
+
+});
